ca9c7334a8482c8a7b2c62e88aff65d205aa6e70
Tim Wray <timwray.mail@gmail.com>
2025-12-19 12:50:43 +0100

    Update starting example

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,9 @@
-import { animatable, createVisualisation } from "./core";
-import { toNormalizedFloat } from "./util";
+import { createVisualisation, logMessage } from "./core";
 
 createVisualisation
-  .setup(({ atStart, onNoteDown, onNoteUp, onRender }) => {
-    atStart(({ visualisation }) => {
-      visualisation.addPermanently(
-        "note",
-        animatable().withRenderer(
-          ({
-            center,
-            circle,
-            withStyles,
-            animate,
-            timeAttacked,
-            timeReleased,
-          }) => {
-            const { x: cx, y: cy } = center;
-
-            // Circle is animatable in response to attack and release
-            // events, startTimes are provided dynamically
-
-            withStyles({ strokeStyle: "#666666" }, () => {
-              circle({
-                cx,
-                cy,
-                radius: animate([
-                  {
-                    startTime: timeAttacked,
-                    from: 50,
-                    to: 100,
-                    duration: 1000,
-                  },
-                  {
-                    startTime: timeReleased,
-                    from: 100,
-                    to: 50,
-                    duration: 1000,
-                  },
-                ]),
-              });
-            });
-          }
-        )
-      );
-    });
-
-    onRender(({ center, circle, withStyles, animate }) => {
-      const { x: cx, y: cy } = center;
-
-      const startYPos = cy - 200;
-      const endYPos = cy - 100;
-
-      // Circles can also be animatable during onRender events
-
-      withStyles({ strokeStyle: "#666666" }, () => {
-        for (let i = 0; i < 3; i++) {
-          circle({
-            cx: cx + i * 40 - 40,
-            cy: animate({
-              from: startYPos,
-              to: endYPos,
-              duration: 1000,
-              delay: 500 + i * 250,
-            }),
-            radius: 10,
-          });
-        }
-      });
-    });
-
-    onNoteDown(({ visualisation }) => {
-      visualisation.get("note")?.attack(toNormalizedFloat(1));
-    });
-
-    onNoteUp(({ visualisation }) => {
-      visualisation.get("note")?.release();
+  .setup(({ atStart }) => {
+    atStart(() => {
+      logMessage("Welcome to Liminalis");
     });
   })
   .render();
